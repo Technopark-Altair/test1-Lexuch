2433af457ff8f159a4ddbf7465afd3d06342ffe9
Lexuch <72134534+Lexuch@users.noreply.github.com>
2020-10-12 19:05:32 +0300

    kr
    .

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kr0'`, () => {
+  it(`should have as title 'country-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('kr0');
+    expect(app.title).toEqual('country-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('kr0 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('country-app app is running!');
   });
 });
